a36fc9bb678abc914fc57f6a2869d25f207c6971
Mark Strendin <mark.strendin@gmail.com>
2019-05-04 18:13:30 -0600

    Initial commit Going to try to get the parsing sorted out first, then worry about how to actually use this library somewhere. This project includes a console app for now, in order to test things.

---
+++ EnvCanadaParserPlayground/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MarkStrendin.EnvCanadaParserPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read in the example file
+            
+            // Run it through the parser
+
+            // Send the results to the console
+        }
+    }
+}
---
+++ EnvCanadaWeatherParser/EnvCanadaWeatherParser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkStrendin.EnvCanadaWeatherParser
+{
+    class EnvCanadaWeatherParser
+    {
+                
+    }
+}
