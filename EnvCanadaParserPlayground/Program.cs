a0718b9562fbeed152b3797195b1323c113c1194
Mark Strendin <mark.strendin@gmail.com>
2019-05-05 00:30:40 -0600

    Grab the live XML RSS feed in the playground project

--- EnvCanadaParserPlayground/Program.cs
+++ EnvCanadaParserPlayground/Program.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MarkStrendin.EnvCanadaParserPlayground
 {
@@ -28,9 +30,28 @@ namespace MarkStrendin.EnvCanadaParserPlayground
                 string exampleContent = reader.ReadToEnd();
                 CurrentWeather result = parser.ParseXML(exampleContent);
                 Console.WriteLine(CurrentWeatherToString(result));
-            }            
+            }
+
+            //https://weather.gc.ca/rss/city/sk-34_e.xml
+
+            Console.WriteLine("Now the live XML feed:");
+            string inputXML = getXMLFeed("https://weather.gc.ca/rss/city/sk-34_e.xml").Result;
+            Console.WriteLine(CurrentWeatherToString(parser.ParseXML(inputXML)));
         }
-        
+
+        private async static Task<string> getXMLFeed(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            using (HttpResponseMessage response = await httpClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();                    
+                }
+            }
+            return string.Empty;
+        }
+
         private static string CurrentWeatherToString(CurrentWeather currentWeather)
         {
             StringBuilder builder = new StringBuilder();
