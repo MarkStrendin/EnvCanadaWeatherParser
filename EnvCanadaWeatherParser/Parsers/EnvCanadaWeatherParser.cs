a7ccf84e095c5c8d193d5567d25eb5c15de17380
Mark Strendin <mark.strendin@gmail.com>
2019-05-06 20:56:49 -0600

    Support French XML files (#6)

--- EnvCanadaWeatherParser/Parsers/EnvCanadaWeatherParser.cs
+++ EnvCanadaWeatherParser/Parsers/EnvCanadaWeatherParser.cs
@@ -8,9 +8,10 @@ namespace MarkStrendin.EnvCanadaWeatherParser
     public class EnvCanadaCurrentWeatherParser
     {
 
-        private const string _locationTitleFluff = " - Weather - Environment Canada";
-        private const string _urlFluff = "https://www.weather.gc.ca/rss/city/";
-        private static readonly string[] _removeFromURLToCreateLocalID = new string[] { "" };
+        private const string _locationTitleFluff_EN = " - Weather - Environment Canada";
+        private const string _locationTitleFluff_FR = " - Météo - Environnement Canada";
+        private const string _urlFluff_EN = "https://www.weather.gc.ca/rss/city/";
+        private const string _urlFluff_FR = "https://www.meteo.gc.ca/rss/city/";
 
         public CurrentWeather ParseXML(string inputXML)
         {
@@ -37,7 +38,7 @@ namespace MarkStrendin.EnvCanadaWeatherParser
                     {
                         if (string.IsNullOrEmpty(returnMe.LocationName))
                         {
-                            if (firstLevelElement.Value.Contains(_locationTitleFluff))
+                            if (firstLevelElement.Value.Contains(_locationTitleFluff_EN) || firstLevelElement.Value.Contains(_locationTitleFluff_FR))
                             {
                                 returnMe.LocationName = parseLocationTitle(firstLevelElement.Value);
                                 fieldsWithFoundValues++;
@@ -54,14 +55,17 @@ namespace MarkStrendin.EnvCanadaWeatherParser
                             {
                                 if (attribute.Name.LocalName == "href")
                                 {
-                                    if (attribute.Value.Contains(_urlFluff))
+                                    if (
+                                        (attribute.Value.Contains(_urlFluff_EN)) ||
+                                        (attribute.Value.Contains(_urlFluff_FR))
+                                        )
                                     {                                        
                                         returnMe.SourceURL = attribute.Value;
                                         returnMe.LocationId = parseLocalIDFromSourceURL(attribute.Value);
                                         fieldsWithFoundValues++;
                                     } else
                                     {
-                                        throw new UnexpectedXMLFormatException("Incorrect XML format - self ref was \"" + attribute.Value + "\", but expected \"" + _urlFluff + "...\"");
+                                        throw new UnexpectedXMLFormatException("Incorrect XML format - self ref was \"" + attribute.Value + "\", but expected \"" + _urlFluff_EN + "...\" or \"" + _urlFluff_FR + "\"");
                                     }
                                 }
                             }
@@ -71,7 +75,10 @@ namespace MarkStrendin.EnvCanadaWeatherParser
                     // Find the "entry" element that has a title that starts with "Current Conditions: "
                     if (firstLevelElement.Name.LocalName.ToLower() == "entry")
                     {
-                        if (firstLevelElement.Value.Contains("Current Conditions: "))
+                        if (
+                            (firstLevelElement.Value.Contains("Current Conditions: ")) ||
+                            (firstLevelElement.Value.Contains("Conditions actuelles: "))
+                            )
                         {
                             foreach (XElement childElement in firstLevelElement.Elements())
                             {
@@ -97,11 +104,14 @@ namespace MarkStrendin.EnvCanadaWeatherParser
                                             fieldsWithFoundValues++;
                                             returnMe.Conditions = currentConditionsElement.Replace("<b>Condition:</b>", string.Empty).Trim();
                                         }
-
-                                        if (currentConditionsElement.StartsWith("<b>Temperature:</b>"))
+                                        
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Temperature:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Température:</b>"))
+                                            )
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.TemperatureCelsius = currentConditionsElement.Replace("<b>Temperature:</b>", string.Empty).Replace("&deg;C", string.Empty).Trim();
+                                            returnMe.TemperatureCelsius = currentConditionsElement.Replace("<b>Température:</b>", string.Empty).Replace("<b>Temperature:</b>", string.Empty).Replace("&deg;C", string.Empty).Trim();
                                             
                                             // Also calculate Fahrenheit conversion
                                             string tempF = "unknown";
@@ -115,16 +125,23 @@ namespace MarkStrendin.EnvCanadaWeatherParser
                                             returnMe.TemperatureKelvin = (celsiusDecimal + (decimal)273.15).ToString();
                                         }
 
-                                        if (currentConditionsElement.StartsWith("<b>Pressure / Tendency:</b>"))
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Pressure / Tendency:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Pression / Tendance:</b>"))
+                                            )
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.Pressure = currentConditionsElement.Replace("<b>Pressure / Tendency:</b>", string.Empty).Trim();
+                                            returnMe.Pressure = currentConditionsElement.Replace("<b>Pression / Tendance:</b>", string.Empty).Replace("<b>Pressure / Tendency:</b>", string.Empty).Trim();
                                         }
 
-                                        if (currentConditionsElement.StartsWith("<b>Wind Chill:</b>"))
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Wind Chill:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Refroidissement éolien:</b>"))
+                                            )
+
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.WindChillCelsius = currentConditionsElement.Replace("<b>Wind Chill:</b>", string.Empty).Trim();
+                                            returnMe.WindChillCelsius = currentConditionsElement.Replace("<b>Refroidissement éolien:</b>", string.Empty).Replace("<b>Wind Chill:</b>", string.Empty).Trim();
                                             // Also calculate Fahrenheit conversion
                                             string windF = "unknown";
                                             if (decimal.TryParse(returnMe.WindChillCelsius, out decimal celsiusDecimal))
@@ -134,28 +151,40 @@ namespace MarkStrendin.EnvCanadaWeatherParser
                                             returnMe.WindChillFahrenheit = windF;
                                         }
 
-                                        if (currentConditionsElement.StartsWith("<b>Pressure:</b>"))
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Pressure:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Pression:</b>"))
+                                            )
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.Pressure = currentConditionsElement.Replace("<b>Pressure:</b>", string.Empty).Trim();
+                                            returnMe.Pressure = currentConditionsElement.Replace("<b>Pression:</b>", string.Empty).Replace("<b>Pressure:</b>", string.Empty).Trim();
                                         }
 
-                                        if (currentConditionsElement.StartsWith("<b>Visibility:</b>"))
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Visibility:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Visibilité:</b>"))
+                                            )
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.Visibility = currentConditionsElement.Replace("<b>Visibility:</b>", string.Empty).Trim();
+                                            returnMe.Visibility = currentConditionsElement.Replace("<b>Visibilité:</b>", string.Empty).Replace("<b>Visibility:</b>", string.Empty).Trim();
                                         }
 
-                                        if (currentConditionsElement.StartsWith("<b>Humidity:</b>"))
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Humidity:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Humidité:</b>"))
+                                            )
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.Humidity = currentConditionsElement.Replace("<b>Humidity:</b>", string.Empty).Trim();
+                                            returnMe.Humidity = currentConditionsElement.Replace("<b>Humidité:</b>", string.Empty).Replace("<b>Humidity:</b>", string.Empty).Trim();
                                         }
 
-                                        if (currentConditionsElement.StartsWith("<b>Dewpoint:</b>"))
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Dewpoint:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Point de rosée:</b>"))
+                                            )
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.DewPointCelsius = currentConditionsElement.Replace("<b>Dewpoint:</b>", string.Empty).Replace("&deg;C", string.Empty).Trim();
+                                            returnMe.DewPointCelsius = currentConditionsElement.Replace("<b>Point de rosée:</b>", string.Empty).Replace("<b>Dewpoint:</b>", string.Empty).Replace("&deg;C", string.Empty).Trim();
 
                                             // Also calculate Fahrenheit conversion
                                             string dewPointF = "unknown";
@@ -166,22 +195,31 @@ namespace MarkStrendin.EnvCanadaWeatherParser
                                             returnMe.DewPointFahrenheit = dewPointF;
                                         }
 
-                                        if (currentConditionsElement.StartsWith("<b>Wind:</b>"))
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Wind:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Vent:</b>"))
+                                            )
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.Wind = currentConditionsElement.Replace("<b>Wind:</b>", string.Empty).Trim();
+                                            returnMe.Wind = currentConditionsElement.Replace("<b>Vent:</b>", string.Empty).Replace("<b>Wind:</b>", string.Empty).Trim();
                                         }
 
-                                        if (currentConditionsElement.StartsWith("<b>Air Quality Health Index:</b>"))
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Air Quality Health Index:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Cote air santé:</b>"))
+                                            )
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.AirQualityHealthIndex = currentConditionsElement.Replace("<b>Air Quality Health Index:</b>", string.Empty).Trim();
+                                            returnMe.AirQualityHealthIndex = currentConditionsElement.Replace("<b>Cote air santé:</b>", string.Empty).Replace("<b>Air Quality Health Index:</b>", string.Empty).Trim();
                                         }
 
-                                        if (currentConditionsElement.StartsWith("<b>Observed at:</b>"))
+                                        if (
+                                            (currentConditionsElement.StartsWith("<b>Observed at:</b>")) ||
+                                            (currentConditionsElement.StartsWith("<b>Enregistrées à:</b>"))
+                                            )
                                         {
                                             fieldsWithFoundValues++;
-                                            returnMe.ObservedAt = currentConditionsElement.Replace("<b>Observed at:</b>", string.Empty).Trim();
+                                            returnMe.ObservedAt = currentConditionsElement.Replace("<b>Enregistrées à:</b>", string.Empty).Replace("<b>Observed at:</b>", string.Empty).Trim();
                                         }
                                     }
 
@@ -203,9 +241,13 @@ namespace MarkStrendin.EnvCanadaWeatherParser
 
         private static string parseLocationTitle(string title)
         {
-            if (title.Contains(_locationTitleFluff))
+            if (title.Contains(_locationTitleFluff_EN))
             {
-                return title.Substring(0, title.IndexOf(_locationTitleFluff)).Trim();
+                return title.Substring(0, title.IndexOf(_locationTitleFluff_EN)).Trim();
+            }
+            else if (title.Contains(_locationTitleFluff_FR))
+            {
+                return title.Substring(0, title.IndexOf(_locationTitleFluff_FR)).Trim();
             }
             else
             {
@@ -215,7 +257,7 @@ namespace MarkStrendin.EnvCanadaWeatherParser
 
         private static string parseLocalIDFromSourceURL(string input)
         {
-            return input.Replace(_urlFluff, string.Empty).Replace("_e.xml", string.Empty).Replace("_f.xml", string.Empty);
+            return input.Replace(_urlFluff_EN, string.Empty).Replace(_urlFluff_FR, string.Empty).Replace("_e.xml", string.Empty).Replace("_f.xml", string.Empty);
         }
 
 
